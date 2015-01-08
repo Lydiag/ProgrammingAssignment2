39de9d4caf8dce1a210ca99e37506d893d771025
Lydiag <lydia.jane@gmail.com>
2015-01-08 09:42:32 +0530

    Added Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Programming assignment
 
-## Write a short comment describing this function
+## makeCacheMatrix has the following functions
+##1. Get the value of the matrix to be inverted
+##2. Set the value of the matrix to be inverted
+##3. Get the inverse of the matrix
+##4. Set the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   invMatrix<-NULL   #initialize inverse matrix to NULL
@@ -28,7 +31,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## CacheSolve does the following functions
+## 1. Get the inverse from makeCacheMatrix function
+## 2. If the inverse matrix is null, the find the inverse using solve() function (in-built)
+## 3. Set the value of inverse in the majeCacheMatrix function -- Caching the inverse
+## 4. If the inverse matrix is not null, then print the cached inverse matrix
 
 cacheSolve <- function(x, ...) {
         inverseMatrix<-x$getInverse()
@@ -43,3 +50,18 @@ cacheSolve <- function(x, ...) {
         inverseMatrix
         
 }
+
+##To check
+##mat<-rbind(c(4,3),c(3,2))
+##cac<-makeCacheMatrix(mat)
+##First run
+##cacheSolve(cac)
+##[,1] [,2]
+##[1,]   -2    3
+##[2,]    3   -4
+##Second Run
+##cacheSolve(cac)
+##Getting the Cached Inverse Matrix.......
+##[,1] [,2]
+##[1,]   -2    3
+##[2,]    3   -4
